baa28f740f9b0e3b28f17355a3519000053cd681
Jean Jia and Steven Colburn <jean@>
2018-06-04 16:04:46 -0700

    give a list of deck cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-var deckStart = ["ace of ","2 of ","3 of ","4 of ","5 of ","6 of ","7 of ","8 of ","9 of ","10 of ","jack of ","queen of ","king of "];
+var deckStart = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
 var deckEnd = ["clubs","diamonds","hearts","spades"];
 var deckFull = [];
 
 $(function(){
 
+  deckEnd.forEach(function(suite){
+    var tempString = "";
+    deckStart.forEach(function(card){
+      tempString = wrapListTag(card+" of "+suite);
+      deckFull.push(tempString);
+    });
+  });
+  $("#deck-list").html(deckFull);
 });
+
+function wrapListTag(value)
+{
+  return ("<li>"+value+"</li>");
+}
